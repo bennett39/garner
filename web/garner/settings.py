ec03c009317173b1f5f0fae874665d5b18137695
Bennett Garner <bennett@cubesoftware.com>
2019-12-08 17:24:22 -0500

    Docker works

--- web/garner/settings.py
+++ web/garner/settings.py
@@ -69,17 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'garner.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'garner_db',
+        'USER': 'web',
+        'PASSWORD': 'root',
+        'HOST': 'garner_db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
